614da9b13eff425bd131f1bed5fd9f265e5984bc
ankush-anonymous <canankushdoit@gmai.com>
2024-09-27 13:02:04 +0530

    dburl update

--- connections/db.js
+++ connections/db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 
 // Database connection URL
 const dbUrl =
-  "postgresql://superjoin_database_user:HsX7lwAEabNcK3ssahaNk7UkBHQ8t5pL@dpg-crqn8tbtq21c73euv720-a.oregon-postgres.render.com:5432/superjoin_database";
+  "postgresql://superjoin_database_1kd6_user:vH8L1O01sI7e8InCkBw7q2e9UiBN3ool@dpg-crr5qrd6l47c73cdi6ng-a.singapore-postgres.render.com:5432/superjoin_database_1kd6";
 
 // Create a new pool using the connection URL
 const pool = new Pool({
